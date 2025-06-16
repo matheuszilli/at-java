f6e00c3688658d1454c48a7e0af28eef4c74c78d
Matheus Zilli <matheus.zilli@al.infnet.edu.br>
2025-06-16 20:59:30 -0300

    Etapa 01 - Exercicio 02

--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -7,7 +7,7 @@ public class HelloController {
 
     /**
      * Endpoint GET /hello
-     * Retonar a mensagem "Hello, Javalin!
+     * Retonar a mensagem "Hello, Javalin!"
      */
     public void hello(Context ctx) {
         ctx.result("Hello, Javalin!");
